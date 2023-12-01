7f5f5f2e193f4563665fc6e20ea7da3296d49791
Nick Logozzo <nlogozzo225@gmail.com>
2023-12-01 16:44:51 -0500

    Update systemcredentials.cpp

--- src/keyring/systemcredentials.cpp
+++ src/keyring/systemcredentials.cpp
@@ -11,7 +11,7 @@
 namespace Nickvision::Aura::Keyring
 {
 #ifndef _WIN32
-	static const SecretSchema KEYRING_SCHEMA = { "org.nickvision.aura.keyring", SECRET_SCHEMA_NONE, { { "application", SECRET_SCHEMA_ATTRIBUTE_STRING }, { NULL, 0 } } };
+	static const SecretSchema KEYRING_SCHEMA = { "org.nickvision.aura.keyring", SECRET_SCHEMA_NONE, { { "application", SECRET_SCHEMA_ATTRIBUTE_STRING }, { "NULL", SECRET_SCHEMA_ATTRIBUTE_STRING } } };
 #endif
 
 	std::optional<Credential> SystemCredentials::getCredential(const std::string& name)
@@ -29,7 +29,7 @@ namespace Nickvision::Aura::Keyring
 		}
 #else
 		GError* error{ nullptr };
-		char* password = secret_password_lookup_sync(&KEYRING_SCHEMA, nullptr, &error, "application", name.c_str(), nullptr);
+		char* password = secret_password_lookup_sync(&KEYRING_SCHEMA, nullptr, &error, "application", name.c_str(), NULL);
 		if (!error && password)
 		{
 			Credential c{ name, "", "default", password };
@@ -65,7 +65,7 @@ namespace Nickvision::Aura::Keyring
 		return c;
 #else
 		GError* error{ nullptr };
-		secret_password_store_sync(&KEYRING_SCHEMA, SECRET_COLLECTION_DEFAULT, c.getName().c_str(), c.getPassword().c_str(), nullptr, &error, "application", c.getName().c_str(), nullptr);
+		secret_password_store_sync(&KEYRING_SCHEMA, SECRET_COLLECTION_DEFAULT, c.getName().c_str(), c.getPassword().c_str(), nullptr, &error, "application", c.getName().c_str(), NULL);
 		if (error)
 		{
 			g_error_free(error);
@@ -81,7 +81,7 @@ namespace Nickvision::Aura::Keyring
 		return CredDeleteA(credential.getName().c_str(), CRED_TYPE_GENERIC, 0);
 #else
 		GError* error{ nullptr };
-		bool res = secret_password_clear_sync(&KEYRING_SCHEMA, nullptr, &error, "application", credential.getName().c_str(), nullptr);
+		bool res = secret_password_clear_sync(&KEYRING_SCHEMA, nullptr, &error, "application", credential.getName().c_str(), NULL);
 		if (!error)
 		{
 			return res;
