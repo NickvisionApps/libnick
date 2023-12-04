c90dbfe36a0a016f630c9b818f8e89c6733bf73c
Nick Logozzo <nlogozzo225@gmail.com>
2023-12-03 22:04:38 -0500

    Keyring - Implement Store

--- include/keyring/store.h
+++ include/keyring/store.h
@@ -1,11 +1,112 @@
 #ifndef STORE_H
 #define STORE_H
 
+#include <filesystem>
+#include <memory>
+#include <optional>
+#include <string>
+#include <vector>
+#include <SQLiteCpp/SQLiteCpp.h>
+#include "credential.h"
+
 namespace Nickvision::Aura::Keyring
 {
+	/**
+	 * @brief A store object for credentials. Backed by sqlciphe.
+	 */
 	class Store
 	{
+	public:
+		/**
+		 * @brief Gets the name of the store.
+		 * @return The name of the store
+		 */
+		const std::string& getName() const;
+		/**
+		 * @brief Gets the file path of the store on disk.
+		 * @return The file path of the store
+		 */
+		const std::filesystem::path& getPath() const;
+		/**
+		 * @brief Gets all credentials in the store.
+		 * @return The list of all credentials
+		 */
+		std::vector<Credential> getAllCredentials() const;
+		/**
+		 * @brief Gets the credential matching the provided id.
+		 * @param id The id of the credential
+		 * @return The credential matching the id, std::nullopt if no matching credential
+		 */
+		std::optional<Credential> getCredential(int id) const;
+		/**
+		 * @brief Gets the credentials containing the provided name.
+		 * @param name The name
+		 * @return The list of credentials matching the name
+		 */
+		std::vector<Credential> getCredentials(const std::string& name) const;
+		/**
+		 * @brief Adds a credential to the store.
+		 * @param credential The Credential to add
+		 * @return True if successful, else false
+		 */
+		bool addCredential(const Credential& credential);
+		/**
+		 * @brief Updates a credential in the store.
+		 * @param credential The Credential to update
+		 * @return True if successful, else false
+		 */
+		bool updateCredential(const Credential& credential);
+		/**
+		 * @brief Deletes a credential from the store.
+		 * @param id The id of the credential to delete
+		 * @return True if successful, else false
+		 */
+		bool deleteCredential(int id);
+		/**
+		 * @brief Destroys the store and all of its data from disk. Once this method is called, the object should no longer be referenced, with or without success.
+		 * @return True if successful, else false
+		 */
+		bool destroy();
 
+	private:
+		Store(const std::string& name, std::unique_ptr<SQLite::Database> database);
+		std::unique_ptr<SQLite::Database> m_database;
+		std::string m_name;
+		std::filesystem::path m_path;
+
+	public:
+		/**
+		 * @brief Gets the directory where stores are saved on disk.
+		 * @return The directory for stores
+		 */
+		static std::filesystem::path getStoreDir();
+		/**
+		 * @brief Creates a new store.
+		 * @param name The name of the store
+		 * @param password The password of the store
+		 * @param overwrite Whether or not to overwrite a store that already exists with the provided name
+		 * @return The created Store object, std::nullopt if creation failed
+		 */
+		static std::optional<Store> create(const std::string& name, const std::string& password, bool overwrite);
+		/**
+		 * @brief Loads a store.
+		 * @param name The name of the store
+		 * @param password The password of the store
+		 * @return The loaded Store object, std::nullopt if loading failed
+		 */
+		static std::optional<Store> load(const std::string& name, const std::string& password);
+		/**
+		 * @brief Gets whether or not a store exists with the provided name.
+		 * @param name The name of the store to check
+		 * @return True if a store with the provied name exists, else false
+		 */
+		static bool exists(const std::string& name);
+		/**
+		 * @brief Destroys a store and all of its data from disk.
+		 * @param The name of the store to destroy
+		 * @return True if successful, else false
+		 */
+		static bool destroy(const std::string& name);
 	};
 }
 
--- src/keyring/store.cpp
+++ src/keyring/store.cpp
@@ -1,6 +1,175 @@
 #include "keyring/store.h"
+#include "userdirectories.h"
+
+using namespace SQLite;
 
 namespace Nickvision::Aura::Keyring
 {
+	std::filesystem::path getPathFromName(const std::string& name)
+	{
+		return Store::getStoreDir() / (name + ".ring");
+	}
 
+	Store::Store(const std::string& name, std::unique_ptr<Database> database)
+		: m_name{ name },
+		m_database{ std::move(database) },
+		m_path{ getPathFromName(name) }
+	{
+		m_database->exec("CREATE TABLE IF NOT EXISTS credentials (id TEXT PRIMARY KEY, name TEXT, uri TEXT, username TEXT, password TEXT)");
+	}
+
+	const std::string& Store::getName() const
+	{
+		return m_name;
+	}
+
+	const std::filesystem::path& Store::getPath() const
+	{
+		return m_path;
+	}
+
+	std::vector<Credential> Store::getAllCredentials() const
+	{
+		std::vector<Credential> creds;
+		Statement query{ *m_database, "SELECT * FROM credentials" };
+		while (query.executeStep())
+		{
+			creds.push_back({ query.getColumn(0).getInt(), query.getColumn(1).getString(), query.getColumn(2).getString(), query.getColumn(3).getString(), query.getColumn(4).getString() });
+		}
+		return creds;
+	}
+
+	std::optional<Credential> Store::getCredential(int id) const
+	{
+		Statement query{ *m_database, "SELECT * FROM credentials where id = ?" };
+		query.bind(1, id);
+		query.executeStep();
+		if (query.hasRow())
+		{
+			return { { query.getColumn(0).getInt(), query.getColumn(1).getString(), query.getColumn(2).getString(), query.getColumn(3).getString(), query.getColumn(4).getString() } };
+		}
+		return std::nullopt;
+	}
+
+	std::vector<Credential> Store::getCredentials(const std::string& name) const
+	{
+		std::vector<Credential> creds;
+
+		Statement query{ *m_database, "SELECT * FROM credentials where name = ?" };
+		query.bind(1, name);
+		while (query.executeStep())
+		{
+			creds.push_back({ query.getColumn(0).getInt(), query.getColumn(1).getString(), query.getColumn(2).getString(), query.getColumn(3).getString(), query.getColumn(4).getString() });
+		}
+		return creds;
+	}
+
+	bool Store::addCredential(const Credential& credential)
+	{
+		Statement query{ *m_database, "INSERT INTO credentials (id, name, uri, username, password) VALUES (?, ?, ?, ?, ?)" };
+		query.bind(1, credential.getId());
+		query.bind(2, credential.getName());
+		query.bind(3, credential.getUri());
+		query.bind(4, credential.getUsername());
+		query.bind(5, credential.getPassword());
+		return query.exec() > 0;
+	}
+
+	bool Store::updateCredential(const Credential& credential)
+	{
+		Statement query{ *m_database, "UPDATE credentials SET name = ?, uri = ?, username = ?, password = ? where id = ?" };
+		query.bind(5, credential.getId());
+		query.bind(1, credential.getName());
+		query.bind(2, credential.getUri());
+		query.bind(3, credential.getUsername());
+		query.bind(4, credential.getPassword());
+		return query.exec() > 0;
+	}
+
+	bool Store::deleteCredential(int id)
+	{
+		Statement query{ *m_database, "DELETE FROM credentials WHERE id = ?" };
+		query.bind(1, id);
+		return query.exec() > 0;
+	}
+
+	bool Store::destroy()
+	{
+		m_database.reset();
+		return std::filesystem::remove(m_path);
+	}
+
+	std::filesystem::path Store::getStoreDir()
+	{
+		return UserDirectories::getConfig() / "Nickvision" / "Keyring";
+	}
+
+	std::optional<Store> Store::create(const std::string& name, const std::string& password, bool overwrite)
+	{
+		std::filesystem::create_directories(getStoreDir());
+		if (name.empty() || password.empty())
+		{
+			return std::nullopt;
+		}
+		std::filesystem::path path{ getPathFromName(name) };
+		if (std::filesystem::exists(path))
+		{
+			if (overwrite)
+			{
+				std::filesystem::remove(path);
+			}
+			else
+			{
+				return std::nullopt;
+			}
+		}
+		try
+		{
+			std::unique_ptr<Database> database{ std::make_unique<Database>(path.string(), OPEN_READWRITE | OPEN_CREATE) };
+			database->key(password);
+			return { { name, std::move(database) } };
+		}
+		catch (...)
+		{
+			return std::nullopt;
+		}
+	}
+
+	std::optional<Store> Store::load(const std::string& name, const std::string& password)
+	{
+		if (name.empty() || password.empty())
+		{
+			return std::nullopt;
+		}
+		std::filesystem::path path{ getPathFromName(name) };
+		if (!std::filesystem::exists(path))
+		{
+			return std::nullopt;
+		}
+		try
+		{
+			std::unique_ptr<Database> database{ std::make_unique<Database>(path.string(), OPEN_READWRITE | OPEN_CREATE) };
+			database->key(password);
+			return { { name, std::move(database) } };
+		}
+		catch (...)
+		{
+			return std::nullopt;
+		}
+	}
+
+	bool Store::exists(const std::string& name)
+	{
+		return std::filesystem::exists(getPathFromName(name));
+	}
+
+	bool Store::destroy(const std::string& name)
+	{
+		std::filesystem::path path{ getPathFromName(name) };
+		if (std::filesystem::exists(path))
+		{
+			return std::filesystem::remove(path);
+		}
+		return false;
+	}
 }
