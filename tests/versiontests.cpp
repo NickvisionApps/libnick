b7fa005288c28cc50cbf1f6829ad9f804ae23e72
Nick Logozzo <nlogozzo225@gmail.com>
2023-12-04 23:30:35 -0500

    Tests - Version Tests

--- tests/versiontests.cpp
+++ tests/versiontests.cpp
@@ -1,8 +1,34 @@
 #include <gtest/gtest.h>
 #include "version.h"
 
+using namespace Nickvision::Aura;
+
 TEST(VersionTests, EmptyVersion) 
 {
-	Nickvision::Aura::Version v;
+	Version v;
 	EXPECT_EQ(v.empty(), true);
+}
+
+TEST(VersionTests, VersionFromStrings)
+{
+	Version v1{ "2023.11.52" };
+	Version v2{ "2022.15.2" };
+	EXPECT_EQ(v1.toString(), "2023.11.52");
+	EXPECT_EQ(v2.toString(), "2022.15.2");
+	EXPECT_EQ(v1 > v2, true);
+	EXPECT_EQ(v2 < v1, true);
+	EXPECT_EQ(v1 == v2, false);
+}
+
+TEST(VersionTests, BetaVersion)
+{
+	Version v{ "2010.5.0-beta1" };
+	EXPECT_EQ(v.getVersionType(), VersionType::Preview);
+}
+
+TEST(VersionTests, BadVersions)
+{
+	EXPECT_THROW(Version(""), std::invalid_argument);
+	EXPECT_THROW(Version("2023"), std::invalid_argument);
+	EXPECT_THROW(Version("2023-11-0"), std::invalid_argument);
 }
