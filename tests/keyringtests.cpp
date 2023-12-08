0999401b57fc4f9f51d421658164a766d1bc783c
Nick Logozzo <nlogozzo225@gmail.com>
2023-12-08 11:19:25 -0500

    Tests - Add Basic KeyringTests

---
+++ tests/keyringtests.cpp
@@ -0,0 +1,18 @@
+#include <gtest/gtest.h>
+#include <iostream>
+#include "keyring/passwordgenerator.h"
+
+using namespace Nickvision::Aura::Keyring;
+
+TEST(KeyringTests, PasswordGen1)
+{
+	PasswordGenerator gen;
+	std::string pass1 = gen.next();
+	std::string pass2 = gen.next();
+	std::string pass3 = gen.next(6);
+	std::count << pass1 << std::endl << pass2 << std::endl << pass3 << std::endl;
+	EXPECT_EQ(pass1.length(), 16);
+	EXPECT_EQ(pass2.length(), 16);
+	EXPECT_EQ(pass3.length(), 6);
+	EXPECT_FALSE(pass1 == pass2);
+}
