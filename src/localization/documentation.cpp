1c8f0937dbcaa16eb865a8adb998a8f2a105b43b
Nick Logozzo <nlogozzo225@gmail.com>
2024-02-21 20:01:02 -0500

    Localization - Remove Boost Dependency

--- src/localization/documentation.cpp
+++ src/localization/documentation.cpp
@@ -1,5 +1,5 @@
 #include "localization/documentation.h"
-#include <boost/locale.hpp>
+#include <locale>
 #include "app/aura.h"
 #include "helpers/stringhelpers.h"
 #include "localization/gettext.h"
@@ -17,33 +17,24 @@ namespace Nickvision::Localization
         }
 #endif
         std::string lang{ "C" };
-        std::string sysLocale{ StringHelpers::split(boost::locale::util::get_system_locale(), ".")[0] }; //split to remove the .UTF-8
-        if (!sysLocale.empty() && sysLocale != "C" && sysLocale != "en_US")
+        std::string sysLocale{ StringHelpers::split(std::locale("").name(), ".")[0] }; //split to remove the .UTF-8
+        if (!sysLocale.empty() && sysLocale != "C" && sysLocale != "en_US" && sysLocale != "*")
         {
             /*
             * Because the translations of a Nickvision application are stored in the application's running
             * directory, we can look at the list of directory names and see if they contain a translation
-            * file (.mo). If a directory contains an mo file, we can add that directory name (which is a 
-            * language code) to the list of available translated languages for the app.
+            * file (.mo) for the sysLocale string. If a directory contains an mo file, we can set the 
+            * documentation lang to the sysLocale. If no directory contains an mo file for the sysLocale, 
+            * we can try to match using the two-letter language code. If that doesn't work, the default
+            * English docs will be used.
             */
-            std::vector<std::string> langs;
+            std::string twoLetter{ StringHelpers::split(sysLocale, "_")[0] };
             for (const std::filesystem::directory_entry& e : std::filesystem::directory_iterator(Aura::getActive().getExecutableDirectory()))
             {
                 if (e.is_directory() && std::filesystem::exists(e.path() / (Gettext::getDomainName() + ".mo")))
                 {
-                    langs.push_back(e.path().filename().string());
-                }
-            }
-            if (std::find(langs.begin(), langs.end(), sysLocale) != langs.end())
-            {
-                lang = sysLocale;
-            }
-            else
-            {
-                std::string twoLetter{ StringHelpers::split(sysLocale, "_")[0] };
-                for (const std::string& l : langs)
-                {
-                    if (l.find(twoLetter) != std::string::npos)
+                    std::string l{ e.path().filename().string() };
+                    if(l == sysLocale || l == twoLetter)
                     {
                         lang = l;
                         break;
