85862630247f82ba51aa7b37969488954f323d86
Nick Logozzo <nlogozzo225@gmail.com>
2024-06-13 17:18:04 -0400

    Update updatertests.cpp

--- tests/updatertests.cpp
+++ tests/updatertests.cpp
@@ -1,7 +1,9 @@
 #include <gtest/gtest.h>
+#include "system/environment.h"
 #include "update/updater.h"
 #include "update/version.h"
 
+using namespace Nickvision::System;
 using namespace Nickvision::Update;
 
 TEST(UpdaterTests, ParabolicStableUpdate)
@@ -12,3 +14,17 @@ TEST(UpdaterTests, ParabolicStableUpdate)
     ASSERT_TRUE(stable.getVersionType() == VersionType::Stable);
     ASSERT_TRUE(stable >= Version("2023.12.0"));
 }
+
+#ifdef _WIN32
+TEST(UpdaterTests, SpotlightWindowsUpdate)
+{
+    if(!Environment::getVariable("GITHUB_ACTIONS").empty())
+    {
+        GTEST_SKIP() << "Skipping Windows update test on GitHub Actions";
+    }
+    Updater updater{ "https://github.com/NickvisionApps/Spotlight" };
+    Version stable{ updater.fetchCurrentVersion(VersionType::Stable) };
+    ASSERT_TRUE(!stable.empty());
+    ASSERT_TRUE(updater.windowsUpdate(VersionType::Stable));
+}
+#endif
