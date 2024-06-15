2a38c3469c9b060a302492e86fbc2e2b4cc2f3fa
Nick Logozzo <nlogozzo225@gmail.com>
2024-06-15 01:03:24 -0400

    App - Fix Linux Build

--- src/app/aura.cpp
+++ src/app/aura.cpp
@@ -13,7 +13,6 @@ using namespace Nickvision::Filesystem;
 using namespace Nickvision::Helpers;
 using namespace Nickvision::Localization;
 using namespace Nickvision::Logging;
-using namespace Nickvision::Notifications;
 using namespace Nickvision::System;
 
 namespace Nickvision::App
@@ -217,11 +216,11 @@ namespace Nickvision::App
     }
 
 #ifdef _WIN32
-    NotifyIcon& Aura::getNotifyIcon(HWND hwnd)
+    Notifications::NotifyIcon& Aura::getNotifyIcon(HWND hwnd)
     {
         if (!m_notifyIcon)
         {
-            m_notifyIcon = std::make_unique<NotifyIcon>(hwnd, StringHelpers::wstr(m_appInfo.getName()), NotifyIconMenu(), true);
+            m_notifyIcon = std::make_unique<Notifications::NotifyIcon>(hwnd, StringHelpers::wstr(m_appInfo.getName()), Notifications::NotifyIconMenu{}, true);
         }
         return *m_notifyIcon;
     }
--- tests/auratests.cpp
+++ tests/auratests.cpp
@@ -1,8 +1,11 @@
 #include <gtest/gtest.h>
+#include <thread>
 #include "app/aura.h"
 #include "app/configurationbase.h"
 #include "app/windowgeometry.h"
 #include "filesystem/userdirectories.h"
+#include "notifications/notifyicon.h"
+#include "notifications/notifyiconmenu.h"
 #include "notifications/shellnotification.h"
 #include "system/environment.h"
 
@@ -142,3 +145,45 @@ TEST_F(AuraTest, HelpUrlChecks)
     ASSERT_EQ(Aura::getActive().getHelpUrl("index"), "help:aura/index");
 #endif
 }
+
+#ifdef _WIN32
+TEST_F(AuraTest, NotifyIconContextMenu)
+{
+    if (GetConsoleWindow())
+    {
+        bool waiting{ true };
+        NotifyIconMenu contextMenu;
+        contextMenu.addAction("Continue", [&waiting]()
+        {
+            waiting = false;
+        });
+        contextMenu.addSeparator();
+        contextMenu.addAction("PLACEHOLDER", [](){});
+        NotifyIcon& notifyIcon{ Aura::getActive().getNotifyIcon(GetConsoleWindow()) };
+        ASSERT_TRUE(notifyIcon.show());
+        ASSERT_TRUE(notifyIcon.setContextMenu(contextMenu));
+        std::cout << "Click \"Continue\" via the context menu of the NotifyIcon to continue..." << std::endl;
+        while (waiting)
+        {
+            MSG msg = { };
+            if(PeekMessageW(&msg, nullptr, 0, 0, PM_REMOVE) > 0)
+            {
+                TranslateMessage(&msg);
+                DispatchMessageW(&msg);
+            }
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        }
+    }
+    ASSERT_TRUE(true);
+}
+
+TEST_F(AuraTest, NotifyIconNotify)
+{
+    if (GetConsoleWindow())
+    {
+        NotifyIcon& notifyIcon{ Aura::getActive().getNotifyIcon(GetConsoleWindow()) };
+        ASSERT_NO_THROW(notifyIcon.notify({ "Test", "Hello from Notifications::NotifyIcon::notify()!", NotificationSeverity::Success }));
+    }
+    ASSERT_TRUE(true);
+}
+#endif
